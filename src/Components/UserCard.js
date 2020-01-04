3fff9e31fb72f41a5ec2dd8d5d48195f1d83930e
tonnac <tonnac35@gmail.com>
2020-01-04 12:12:10 +0900

    8.2 Search Screen UI Part Two

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -4,13 +4,31 @@ import PropTypes from "prop-types";
 import Avatar from "./Avatar";
 import FatText from "./FatText";
 import Button from "./Button";
+import { Link } from "react-router-dom";
 
-const Card = styled.div``;
+const Card = styled.div`
+  ${props => props.theme.whiteBox}
+  display : flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`;
+
+const EAvatar = styled(Avatar)`
+  margin-bottom: 15px;
+`;
+
+const ELink = styled(Link)`
+  color: inherit;
+  margin-bottom: 10px;
+`;
 
 const UserCard = ({ username, isFollowing, url, isSelf }) => (
   <Card>
-    <Avatar url={url} />
-    <FatText text={username} />
+    <EAvatar url={url} size="md" />
+    <ELink to={`/${username}`}>
+      <FatText text={username} />
+    </ELink>
     {!isSelf && <Button text={isFollowing ? "Unfollow" : "Follow"} />}
   </Card>
 );
@@ -18,7 +36,7 @@ const UserCard = ({ username, isFollowing, url, isSelf }) => (
 UserCard.propTypes = {
   username: PropTypes.string.isRequired,
   isFollowing: PropTypes.bool.isRequired,
-  url: PropTypes.string.isRequired,
+  url: PropTypes.string,
   isSelf: PropTypes.bool.isRequired
 };
 
